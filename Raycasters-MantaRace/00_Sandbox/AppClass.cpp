96ff5537208d6658e2975287044627e97721422a
unknown <jja6071@NM2550-14.main.ad.rit.edu>
2015-10-25 15:25:36 -0400

    Another try to fix a problem with include folder

--- Raycasters-MantaRace/00_Sandbox/AppClass.cpp
+++ Raycasters-MantaRace/00_Sandbox/AppClass.cpp
@@ -41,8 +41,6 @@ void AppClass::Update(void)
 
 	m_pMeshMngr->SetModelMatrix(rotX * rotY * rotZ, "Creeper");
 
-	//DELETE ME!
-
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
 }

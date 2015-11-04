ae7b30cc1096c474b61672cf1c2f99b3177b839c
unknown <bmb1829@GDD-38.main.ad.rit.edu>
2015-11-04 09:49:03 -0500

    added get methods to .h

--- MantaRace/00_Sandbox/BoundingObject.h
+++ MantaRace/00_Sandbox/BoundingObject.h
@@ -6,21 +6,6 @@
 
 class BoundingObject
 {
-public:
-	BoundingObject();
-	~BoundingObject();
-
-	BoundingObject(std::vector<vector3> pts);
-
-	void Init();
-	void SetModelMatrix(matrix4 _m4ToWorld);
-	vector3 GetRadiusV3();
-	vector3 maxDist;
-	vector3 minDist;
-	float GetRadiusF();
-
-	bool IsBoxColliding(BoundingObject* other);
-	bool IsSphereColliding(BoundingObject* other);
 
 	matrix4 m_m4ToWorld = IDENTITY_M4; //Matrix that will take us from local to world coordinate
 	vector3 m_v3Center = vector3(0.0f); //Will store the center point of the box Class
@@ -33,6 +18,26 @@ public:
 
 	vector3 m_v3HalfWidth = vector3(0.0f);//Will store half the size of all sides
 	vector3 m_v3HalfWidthG = vector3(0.0f);//Will store half the size of all sides
+
+
+public:
+	BoundingObject();
+	~BoundingObject();
+
+	BoundingObject(std::vector<vector3> pts);
+
+	void Init();
+	void SetModelMatrix(matrix4 _m4ToWorld);
+	vector3 GetRadiusV3();
+	float GetRadiusF();
+	matrix4 GetModelMatrix();
+	vector3 GetCenterLocal();
+	vector3 GetCenterGlobal();
+	vector3 GetHalfWidthGlobal();
+	vector3 GetHalfWidthLocal();
+	
+	bool IsBoxColliding(BoundingObject* other);
+	bool IsSphereColliding(BoundingObject* other);
 
 };
 #endif //__MYBOUNDINGBOXCLASS_H__

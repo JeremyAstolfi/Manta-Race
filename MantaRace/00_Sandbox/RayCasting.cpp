0bd51b78a58a7678d8d17b87236ec1cffec93c75
Rygaido <rygaido@gmail.com>
2015-11-11 20:09:05 -0500

    Finished TriCollision raycast method
    And made a Box Collision method that basically calls Tri 12 times

--- MantaRace/00_Sandbox/RayCasting.cpp
+++ MantaRace/00_Sandbox/RayCasting.cpp
@@ -90,6 +90,7 @@ vector3* RayCasting::TriangleCollision(vector3 position, vector3 direction, vect
 
 	float d = norm.x*p1.x + norm.y*p1.y + norm.z*p1.z;
 
+	//find where ray intersects plane
 	vector3* intersection = PlaneCollision(position, direction, norm, d);
 
 	//if ray does not intersect with triangles plane, it can't possibly intersect triangle
@@ -98,13 +99,115 @@ vector3* RayCasting::TriangleCollision(vector3 position, vector3 direction, vect
 		//determine if point is within triangle
 		//attempting "Barycentric Technique"
 
+		//get two sides of the triangle from point 1
+		vector3 s1 = p2 - p1;
+		vector3 s2 = p3 - p1;
+		//line between point 1 and intersection
+		vector3 mid = *intersection - p1;
+
+		//find dots for each vector by each vector
+		float dot11 = glm::dot(s1, s1);
+		float dot12 = glm::dot(s1, s2);
+		float dot1m = glm::dot(s1, mid);
+		float dot22 = glm::dot(s2, s2);
+		float dot2m = glm::dot(s2, mid);
+
+		//the common denominator for two following equations
+		float denom = (dot11 * dot22 - dot12 * dot12);
+		//projecting mid onto each side
+		float ps1 = (dot22 * dot1m - dot12 * dot2m) / denom;
+		float ps2 = (dot11 * dot2m - dot12 * dot1m) / denom;
+		//these projections repesent how far along the side that point is on a scale of 0 to 1
+		//1 being the far corner and 0 being corner 1. a ps greater than 1 is beyond the far corner, and negative is behind corner 1 (both mean out of triangle)
+		//basically, if this were a right triangle set on the origin, this would check if mid's x and y fall into the height and width
+		//if the sum of these projections is greater than 1, than mid is too long and extends beyond the far side of the triangle
+		//if either projection is greater than 1, so will the sum, these don't need to be checked individually
+		if (ps1 >= 0 && ps2 >= 0 && ps1 + ps2 < 1){
+			//previously found intersection point is within triangle, return it as triangle's intersection point
+			return intersection;
+		}
 	}
 
-	//return nullptr;
-
+	//is ray either does not intersect plane, or does so outside the triangle, return null for no intersection with triangle
 	return intersection;
 }
 
 //BoxCollision
 //call tricollision 12 times?
 //create squarecollision and call 6 times?
+vector3* RayCasting::BoxCollision(vector3 position, vector3 direction, std::vector<vector3> verts){
+	if (verts.size() != 8){ //not a box, ray doesn't collide with a box
+		return nullptr;    //I hope to make a more versatile 3d object collision method in the future, but for now box is strictly box
+	}
+
+	//test every tri in box, if any return an intersection point return it, no need to keep checking other tris
+	//topside
+	vector3* triTest= TriangleCollision(position, direction, verts[0], verts[1], verts[2]);
+	if (triTest!=nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[1], verts[2], verts[3]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//frontside
+	triTest = TriangleCollision(position, direction, verts[0], verts[1], verts[6]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[1], verts[6], verts[5]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//leftside
+	triTest = TriangleCollision(position, direction, verts[0], verts[3], verts[8]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[3], verts[8], verts[5]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//backside
+	triTest = TriangleCollision(position, direction, verts[2], verts[3], verts[8]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[3], verts[8], verts[7]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//rightside
+	triTest = TriangleCollision(position, direction, verts[1], verts[2], verts[7]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[2], verts[7], verts[6]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//bottomside
+	triTest = TriangleCollision(position, direction, verts[5], verts[6], verts[7]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+	triTest = TriangleCollision(position, direction, verts[6], verts[7], verts[8]);
+	if (triTest != nullptr){
+		return triTest;
+	}
+
+	//if no intersection found by now, return null
+	return nullptr;
+
+	//I imagine for a box it would be twice as effective to make a QuadCollision method
+	//but I think that's the extend of a quadCollision method, tris are more useable for complex objects
+
+	//this is my understanding of raycasting so far,it's possible I will have to change my approach when I make the complex object collision method
+}
+
+//square collision?
